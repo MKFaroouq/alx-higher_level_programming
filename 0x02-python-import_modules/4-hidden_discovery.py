71beabe915d54fc86ce12f5f79335dc48f2705b3
MKFaroouq <iprobazooka@gmail.com>
2024-04-08 15:58:36 +0200

    Tears Don't Fall

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+    functions_name = dir(hidden_4)
+    for name in functions_name:
+        if name[0] != '_' and name[1] != '_':
+            print(name)
