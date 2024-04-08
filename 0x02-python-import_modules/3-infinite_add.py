2ba78b645723a9db0b90fe0d8557a5109d2d4bf7
MKFaroouq <iprobazooka@gmail.com>
2024-04-08 15:51:10 +0200

    Tears Don't Fall

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    argvs = sys.argv
+    from sys import argv
     sum = 0
-    for arg in argvs:
-        if arg != argvs[0]:
-            sum = sum + int(arg)
+    for i in range(1, len(argv)):
+        sum = sum + int(argv[i])
     print(sum)
