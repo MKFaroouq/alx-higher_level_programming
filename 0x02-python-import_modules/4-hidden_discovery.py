2b3dd186babb39fc1a76364725655cc0fa14e84f
MKFaroouq <iprobazooka@gmail.com>
2024-04-09 00:41:46 +0200

    4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+    functions_name = dir(hidden_4)
+    for name in functions_name:
+        if name[0] != '_' and name[1] != '_':
+            print(name)
