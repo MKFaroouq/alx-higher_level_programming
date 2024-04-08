d34e40c36467f637df979b2488f78c23c48c7f30
MKFaroouq <iprobazooka@gmail.com>
2024-04-08 15:48:45 +0200

    Tears Don't Fall

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argvs = sys.argv
+    sum = 0
+    for arg in argvs:
+        if arg != argvs[0]:
+            sum = sum + int(arg)
+    print(sum)
