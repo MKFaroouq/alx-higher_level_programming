fc768abf625399d65e3ec846c5589b69573f912c
MKFaroouq <iprobazooka@gmail.com>
2024-04-13 03:50:16 +0200

    1-element_at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
         for i in my_list:
-            if my_list.index(i):
+            if my_list.index(i) == idx:
                 return i
