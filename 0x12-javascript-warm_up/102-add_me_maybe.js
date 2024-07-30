2f23bb0008604ac9a540d1c5176989ac0871bc9b
MKFaroouq <iprobazooka@gmail.com>
2024-07-30 20:41:00 +0300

    102-add_me_maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-
-myObject.incr = function incr () {
-  myObject.value += 1;
-};
-
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+function addMeMaybe (number, theFunction) {
+  number++;
+  theFunction(number);
+}
+module.exports = { addMeMaybe };
