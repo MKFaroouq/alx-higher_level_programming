b3f5559f6dbb7543d811098483a9daadc1fd182b
MKFaroouq <iprobazooka@gmail.com>
2024-07-30 20:35:13 +0300

    13-add

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
-exports.add = function (num1, num2) {
-    return num1 + num2;
-};
+
+function add (a, b) {
+  if (!isNaN(a)) {
+    a = parseFloat(a);
+  }
+  if (!isNaN(b)) {
+    b = parseFloat(b);
+  }
+  return (a + b);
+}
+module.exports = { add };
