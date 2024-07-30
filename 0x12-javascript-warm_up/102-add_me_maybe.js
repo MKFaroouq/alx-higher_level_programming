df8051bb9cc3f2d20329b359a2e8aec0ea719c84
MKFaroouq <iprobazooka@gmail.com>
2024-07-30 20:39:10 +0300

    102-add_me_maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-    theFunction(number + 1);
-  };
+function addMeMaybe (number, theFunction) {
+  number++;
+  theFunction(number);
+}
+module.exports = { addMeMaybe };
