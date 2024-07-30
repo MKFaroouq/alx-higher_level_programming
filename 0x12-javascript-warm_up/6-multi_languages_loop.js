230f77d6bd8f264de461fc3548fdcce0142294e9
MKFaroouq <iprobazooka@gmail.com>
2024-07-30 20:03:06 +0300

    6-multi_languages_loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const arr = ['C is fun' , 'Python is cool' , 'JavaScript is amazing'];
-let i=0;
-for (let i=0; i<3; i++){
-    console.log(arr[i])
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
 }
